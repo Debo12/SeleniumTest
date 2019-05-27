a5c90f9fffbdc9b4b45dca5586ee98b972819361
Debojyoti Dutta <debojyotidutta94@gmail.com>
2019-05-27 23:55:15 +0530

    commit all

---
+++ AutomationTestingProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationTestingProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomationTestingProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f5497d2-71b1-47d5-8942-871dc8d94e8a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AutomationTestingProject/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationTestingProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private TestContext testContextInstance;
+        private IWebDriver driver;
+        private string appURL;
+
+        public UnitTest1() { }
+
+        [TestInitialize]
+        public void SetUpTest()
+        {
+            appURL = "https://www.google.com/";
+            string browser = "Chrome";
+
+            switch (browser)
+            {
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    break;
+
+                default:
+                    driver = new ChromeDriver();
+                    break;
+            }
+        }
+
+        //http:/executeautomation.com/blog/tag/testcontext/
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void GoogleSearchResult()
+        {
+            driver.Navigate().GoToUrl(appURL + "/");
+            //driver.FindElement(By.Id("sb_form_q")).SendKeys("Azure Pipelines");
+            driver.FindElement(By.XPath("//*[@title='Search']")).SendKeys("Selenium");
+            Thread.Sleep(500);
+            driver.FindElement(By.XPath("//*[@value='Google Search']")).Click();
+            Assert.IsTrue(driver.Title.Contains("Selenium"), "Verified title of the page");
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            driver.Quit();
+        }
+
+    }
+}
